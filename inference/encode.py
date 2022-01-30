fixed bug: (1) encoding lost/missed some data; (2) the pyserini implementation of SPLADE (query encoder) did not input attention mask for MLM forward. Now we can reproduce the results of distilsplade_max on SciFact

inference/encode.py
@@ -27,6 +27,7 @@ def one_process(
     chunk_size: int,
     show_progress_bar: bool
 ):
+    nlines_encoded = 0
     encode_fn = encode_fn_builder(gpu)
     batch_ids = []
     batch_texts = []
@@ -43,7 +44,7 @@ def one_process(
         batch_ids.append(example['id'])
         batch_texts.append(example['text'])
 
-        if len(batch_ids) >= batch_size or i == end - 1:
+        if len(batch_ids) >= batch_size or len(batch_ids) + len(term_weights_chunk) >= chunk_size or i == end - 1:
             with torch.no_grad():
                 term_weights_batch = encode_fn(batch_texts)
 
@@ -55,20 +56,24 @@ def one_process(
                 } for doc_id, text, term_weights in zip(batch_ids, batch_texts, term_weights_batch)
             ]
             term_weights_chunk.extend(term_weights_batch)
-            
+
             if len(term_weights_chunk) >= chunk_size or i == end - 1:
-                chunk_idx = (i + 1) // chunk_size - 1
+                chunk_idx = math.ceil((i + 1) / chunk_size) - 1
                 fname_save = f'split{chunk_idx}.jsonl.gz'
+                print(f'Writing to {fname_save}. i: {i}, range: {data_range}')
                 
                 os.makedirs(output_dir, exist_ok=True)
-                with gzip.open(os.path.join(output_dir, fname_save), 'wt') as f:
+                with gzip.open(os.path.join(output_dir, fname_save), 'at+') as f:
                     for line in term_weights_chunk:
                         f.write(json.dumps(line) + '\n')
-
+                        nlines_encoded += 1
+                
                 term_weights_chunk = []
 
             batch_ids = []
             batch_texts = []
+    
+    print(f'Encoded {nlines_encoded} lines. The range is {data_range}')
 
 def _run(
     encode_fn_builder: Callable[[int], Callable[[List[str],], Dict[str, object]]],
@@ -80,10 +85,16 @@ def _run(
 ):
     nexamples = len(data_iter)
     assert len(set(gpus)) == len(gpus)
-    range_size = math.ceil(nexamples / len(gpus))
+    if os.path.exists(output_dir):
+        assert len(os.listdir(output_dir)) == 0, 'Please empty the output_dir'
+
+    range_size = nexamples // len(gpus)
     chunk_size = min(chunk_size, range_size-1)  # important for saving all the encoded data !!
     
     ranges = [(b, b+range_size) for b in range(0, nexamples, range_size)]
+    ranges[len(gpus)-1] = (ranges[len(gpus)-1][0], ranges[-1][1])  # merge all the leftovers into the last range
+    ranges = ranges[:len(gpus)]  # keep only #GPUs ranges
+
     ranges[-1] = (ranges[-1][0], min(ranges[-1][1], nexamples))  # make sure the last range is right, which is important for the criterion of ending point
     if len(gpus) > 1:
         ps = [mp.Process(target=one_process, args=(
@@ -128,7 +139,7 @@ if __name__ == '__main__':
     parser.add_argument('--data_dir')
     parser.add_argument('--gpus', nargs='+', type=int)
     parser.add_argument('--output_dir')
-    parser.add_argument('--batch_size', default=64)
-    parser.add_argument('--chunk_size', default=100000)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--chunk_size', type=int, default=100000)
     args = parser.parse_args()
     run(**vars(args))