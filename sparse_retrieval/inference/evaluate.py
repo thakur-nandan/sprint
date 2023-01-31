0e8d7998e35ac05aaae2f45aa1f2c303921c38ad
Nandan Thakur <nandant@gmail.com>
2023-01-31 11:48:14 -0500

    remove self ids by default during evaluation for ArguAna. Resolves #2

--- sparse_retrieval/inference/evaluate.py
+++ sparse_retrieval/inference/evaluate.py
@@ -3,6 +3,7 @@ import csv
 import json
 import os
 from typing import Dict
+from .utils import load_qrels
 from beir.retrieval.evaluation import EvaluateRetrieval
 
 
@@ -15,37 +16,22 @@ def load_results(result_path, format) -> Dict[str, Dict[str, float]]:
         with open(result_path, 'r') as f:
             for line in f:
                 qid, doc_id, rank = line.strip().split('\t')
-                pseudo_score = 1 / int(rank)
-                results.setdefault(qid, {})
-                results[qid][doc_id] = pseudo_score
+                if str(qid) != str(doc_id): 
+                    pseudo_score = 1 / int(rank)
+                    results.setdefault(qid, {})
+                    results[qid][doc_id] = pseudo_score
     elif format == 'trec':
         with open(result_path, 'r') as f:
             for line in f:
                 qid, _, doc_id, rank, score, _ = line.strip().split()
-                score = float(score)
-                results.setdefault(qid, {})
-                results[qid][doc_id] = score
+                if str(qid) != str(doc_id): 
+                    score = float(score)
+                    results.setdefault(qid, {})
+                    results[qid][doc_id] = score
     else:
         raise NotImplementedError
     
     return results
-
-def load_qrels(qrels_path) -> Dict[str, Dict[str, float]]:
-    # adapted from BeIR: 
-    # https://github.com/UKPLab/beir/blob/568f4c34fa0be1901e2aaa8479978c9e54a1e377/beir/datasets/data_loader.py#L114
-    reader = csv.reader(open(qrels_path, encoding="utf-8"), delimiter="\t", quoting=csv.QUOTE_MINIMAL)
-    next(reader)  # skip header
-    
-    qrels = {}
-    for row in reader:
-        query_id, corpus_id, score = row[0], row[1], int(row[2])
-        
-        if query_id not in qrels:
-            qrels[query_id] = {corpus_id: score}
-        else:
-            qrels[query_id][corpus_id] = score
-    
-    return qrels
 
 def run(result_path, format, qrels_path, output_dir, k_values=[1,3,5,10,100,1000]):
     results = load_results(result_path, format)
