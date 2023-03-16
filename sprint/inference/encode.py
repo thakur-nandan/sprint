861ca219d29205fba23bbaf555515e3711351d84
Nandan Thakur <nandant@gmail.com>
2023-03-16 14:38:07 -0400

    updated argparse arguments and added logging

--- sprint/inference/encode.py
+++ sprint/inference/encode.py
@@ -1,5 +1,5 @@
 from multiprocessing import Value
-from typing import Callable, List, Dict, Iterable, Tuple, Union
+from typing import Callable, List, Iterable, Tuple, Union
 from torch import multiprocessing as mp
 try:
      mp.set_start_method('spawn')  # needed by CUDA + multiprocessing
@@ -12,11 +12,13 @@ import math
 import json
 import tqdm
 import argparse
+import logging
 import copy
 from . import data_iters
 from . import encoder_builders
 from pyserini.encode import QueryEncoder, DocumentEncoder
 
+logger = logging.getLogger(__name__)
 
 def one_process(
     encoder_builder: Callable[[int], Union[QueryEncoder, DocumentEncoder]], 
@@ -39,9 +41,9 @@ def one_process(
         if not (begin <= i < end):
             continue
         
-        assert type(example) == dict
-        assert 'id' in example
-        assert 'text' in example
+        assert type(example) == dict, f'Example must be a dict, but got {type(example)}'
+        assert 'id' in example, f'Example must have an id, but got {example}'
+        assert 'text' in example, f'Example must have a text, but got {example}'
 
         batch_ids.append(example['id'])
         batch_texts.append(example['text'])
@@ -62,7 +64,7 @@ def one_process(
             if len(term_weights_chunk) >= chunk_size or i == end - 1:
                 chunk_idx = math.ceil((i + 1) / chunk_size) - 1
                 fname_save = f'split{chunk_idx}.jsonl.gz'
-                print(f'Writing to {fname_save}. i: {i}, range: {data_range}')
+                logger.info(f'Writing to {fname_save}. i: {i}, range: {data_range}')
                 
                 os.makedirs(output_dir, exist_ok=True)
                 with gzip.open(os.path.join(output_dir, fname_save), 'at+') as f:
@@ -75,7 +77,7 @@ def one_process(
             batch_ids = []
             batch_texts = []
     
-    print(f'Encoded {nlines_encoded} lines. The range is {data_range}')
+    logger.info(f'Encoded {nlines_encoded} lines. The range is {data_range}')
     return_value.value = nlines_encoded
 
 def _run(
@@ -142,18 +144,18 @@ def run(encoder_name, ckpt_name, data_name, data_dir, gpus, output_dir, batch_si
     data_iter = data_iters.build(data_name, data_dir)
     torch.multiprocessing.freeze_support()
     _run(encoder_builder, data_iter, gpus, output_dir, batch_size, chunk_size)
-    print(f'{__name__}: Done')
+    logger.info(f'{__name__}: Done')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--encoder_name')
-    parser.add_argument('--ckpt_name')
-    parser.add_argument('--data_name')
-    parser.add_argument('--data_dir')
-    parser.add_argument('--gpus', nargs='+', type=int)
-    parser.add_argument('--output_dir')
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--chunk_size', type=int, default=100000)
+    parser.add_argument('--encoder_name', type=str, choices=["unicoil", "splade", "sparta", "tildev2", "deepimpact"], help="The name of the sparse encoder. For example, splade, unicoil, etc.")
+    parser.add_argument('--ckpt_name', type=str, help="The name of the sparse model checkpoint.")
+    parser.add_argument('--data_name', type=str, help="The name of the dataset. For example, scifact, trec-covid, etc.")
+    parser.add_argument('--data_dir', type=str, help="The directory where the BEIR data is present. For example, if you are using the scifact dataset, this is the directory where the corpus.jsonl is stored.")
+    parser.add_argument('--gpus', nargs='+', type=int, help="The gpus to use. For example, --gpus 0 1 2 3")
+    parser.add_argument('--output_dir', type=str, help="The output directory where the encoded data will be saved.")
+    parser.add_argument('--batch_size', type=int, default=64, help="Default is 64. This is the number of documents to be encoded at a time. If you have a large GPU, you can increase this number to speed up the encoding process.")
+    parser.add_argument('--chunk_size', type=int, default=100000, help="Default is 100k. This is the number of documents to be encoded at a time. If you have a large GPU, you can increase this number to speed up the encoding process.")
     args = parser.parse_args()
     run(**vars(args))
