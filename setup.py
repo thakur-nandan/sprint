c646b153a43da255054c5ec2d532babed4a57b01
Nandan Thakur <nandant@gmail.com>
2023-03-31 12:16:33 -0400

    Updated setup.py with latest changes

--- setup.py
+++ setup.py
@@ -5,23 +5,30 @@ with open("README.md", "r", encoding="utf-8") as fh:
     readme = fh.read()
 
 setup(
-    name="sparse-retrieval",
+    name="sprint",
     version="0.0.1",
-    author="",
-    author_email="",
-    description="",
+    author="Nandan Thakur",
+    author_email="nandant@gmail.com",
+    description="SPRINT: Sparse Retrieval Neural Toolkit",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/NThakur20/sparse-retrieval",
+    url="https://github.com/thakur-nandan/sprint",
     project_urls={
-        "Bug Tracker": "https://github.com/NThakur20/sparse-retrieval/issues",
+        "Bug Tracker": "https://github.com/thakur-nandan/sprint/issues",
     },
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Beta",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Intended Audience :: Science/Research",
     ],
-    python_requires=">=3.6",
-    install_requires=[],
+    python_requires=">=3.8",
+    install_requires=[
+        "beir",
+        "pyserini"
+    ],
+    keywords="Information Retrieval Toolkit Sparse Retrievers Networks BERT PyTorch IR NLP deep learning"
 )
